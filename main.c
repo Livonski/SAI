6a33cafe944a4c388fcb1574262f8ec86b99a4aa
Livonski <rozernoj@gmail.com>
2026-09-17 18:55:53 +0300

    refactored how neurons are stored, now all neurons are stored inside big neuron blob

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <time.h>
+
 #include <assert.h>
 #include <stdbool.h>
 
 #define da_append(da, e)\
 do{\
+    if(da.capaticy == 0){\
+        da.capaticy = 256;\
+        da.items = malloc(da.capaticy * sizeof(*da.items));\
+    }\
     if(da.count >= da.capaticy){\
         if(da.capaticy == 0) da.capaticy = 256;\
         else da.capaticy *= 2;\
@@ -17,6 +23,10 @@ do{\
 
 #define da_appendP(da, e)\
 do{\
+    if(da->capaticy == 0){\
+        da->capaticy = 256;\
+        da->items = malloc(da->capaticy * sizeof(*da->items));\
+    }\
     if(da->count >= da->capaticy){\
         if(da->capaticy == 0) da->capaticy = 256;\
         else da->capaticy *= 2;\
@@ -27,11 +37,21 @@ do{\
 
 #define da_init(type) malloc(256 * sizeof(type))
 
-#define layerMax(net, i) i == net.layerIndexes.count - 1 ? (net.neurons.count - 1) : (net.layerIndexes.items[i + 1] - 1)
-#define layerMaxP(net, i) i == net->layerIndexes.count - 1 ? (net->neurons.count - 1) : (net->layerIndexes.items[i + 1] - 1)
+#define layerMax(net, i) (((i) == (net).layerIndexes.count - 1) ? ((net).neurons.count - 1) : ((net).layerIndexes.items[(i) + 1] - 1))
+
+#define layerStartP(net, i) (net->layerIndexes.items[i])
+#define layerMaxP(net, i) (((i) == (net)->layerIndexes.count - 1) ? ((net)->neurons.count - 1) : ((net)->layerIndexes.items[(i) + 1] - 1))
 
 #define randM11() ((double)rand()/RAND_MAX - 0.5f) * 2
 #define randM0505() ((double)rand()/RAND_MAX - 0.5f)
+
+#define randf(min, max) ((min) + ((float)rand() / (float)RAND_MAX) * ((max) - (min)))
+
+typedef enum{
+    af_none,
+    af_ReLu,
+    af_sigmoid
+}activationFunction;
 
 typedef struct{
     int* items;
@@ -41,7 +61,7 @@ typedef struct{
 
 typedef struct{
     float value;
-    float valueAA;
+    activationFunction aF;
 
     float gradient;
 
@@ -74,18 +94,13 @@ typedef struct{
 } numbersf;
 
 typedef struct {
-    neurons inputLayer;
-
     neurons neurons;
     layerIndexes layerIndexes;
-
-    neurons outputLayer;
 
     weights weights;
 
     numbersf weightGradients;
 
-    
     float prediction;
     float target; 
     float error;
@@ -109,10 +124,6 @@ typedef struct{
     int capaticy;
 } trainingData;
 
-typedef enum{
-    af_ReLu,
-    af_sigmoid
-}activationFunction;
 
 void GenerateTrainingData(trainingData *tData, int tDataCount){
     //trainingData tData = da_init(dataSample);
@@ -140,81 +151,58 @@ float sigmoid(float v){
     return 1.0f / (1.0f + expf(-v));
 }
 
-float activate(float v, activationFunction func){
-    switch(func){
-        case 0:
-        return relu(v);
-        break;
-        case 1:
-        return sigmoid(v);
-        break;
+float activate(neuron* n){
+    switch(n->aF){
+        case af_ReLu:
+            return relu(n->value);
+            break;
+        case af_sigmoid:
+            return sigmoid(n->value);
+            break;
+        case af_none:
+            return n->value;
+            break;
         default:
-        assert(false && "Unknown activation function type " && func);
-        break;
+            assert(false && "Unknown activation function type " && n->aF);
+            break;
     }
     return 0;
 }
 
-void createRandomNeurons(neuralNetwork *nn, int nCount){
+void createRandomNeurons(neuralNetwork *nn, int nCount, float minBias, float maxBias, activationFunction aF){
     for(int i = 0; i < nCount; i++){
-        float randBias = randM11();
-        neuron nNeuron = {.value = 0, .bias = randBias, .wIndexStart = -1};
+        float randBias = randf(minBias, maxBias);
+        neuron nNeuron = {.value = 0, .bias = randBias, .wIndexStart = -1, .aF = aF};
         da_append(nn->neurons, nNeuron);
     }
 }
 
 void GenerateRandomNeuralNetwork(neuralNetwork *nn){
     //input layer
-    neuron emptyNeuron = {.value = 1.0f, .valueAA = 1.0f, .bias = 0.0f, .wIndexStart = -1};
-    for(int i = 0; i < 2; i++){
-        da_append(nn->inputLayer, emptyNeuron);
-    }
-    
+    da_append(nn->layerIndexes, nn->neurons.count);
+    createRandomNeurons(nn, 2, 0.0f, 0.0f, af_none);
+
     //inner layers
     da_append(nn->layerIndexes, nn->neurons.count);
-    createRandomNeurons(nn, 3);
+    createRandomNeurons(nn, 3, 0.0f, 1.0f, af_ReLu);
     
     da_append(nn->layerIndexes, nn->neurons.count);
-    createRandomNeurons(nn, 2);
+    createRandomNeurons(nn, 2, 0.0f, 1.0f, af_ReLu);
     
     //output layer
-    for(int i = 0; i < 1; i++){
-        da_append(nn->outputLayer, emptyNeuron);
-    }
+    da_append(nn->layerIndexes, nn->neurons.count);
+    createRandomNeurons(nn, 1, 0.0f, 0.0f, af_sigmoid);
 
     //weight construction
-    for(int i = 0; i <= nn->layerIndexes.count; i++){
-        if(i == 0){
-            int layerMax = layerMaxP(nn, i);
-            for(int j = nn->layerIndexes.items[i]; j <= layerMax; j++){
-                nn->neurons.items[j].wIndexStart = nn->weights.count;
-                for(int k = 0; k < nn->inputLayer.count; k++){
-                    printf("Connection - IL(%d) -> Layer(%d:%d)\n", k, i, j);
-                    da_append(nn->weights, randM0505());
-                }
-            }
-        } else if (i == nn->layerIndexes.count){
-            int layerMax = layerMaxP(nn, i - 1);
-            for(int j = 0; j < nn->outputLayer.count; j++){
-                nn->outputLayer.items[j].wIndexStart = nn->weights.count;
-                for(int k = nn->layerIndexes.items[i - 1]; k <= layerMax; k++){
-                    printf("Connection - Layer(%d:%d) -> OL(%d)\n", i - 1, k, j);
-                    da_append(nn->weights, randM0505());
-                }
-            }
-        } else {
-            int layerMax = layerMaxP(nn, i);
-            for(int j = nn->layerIndexes.items[i]; j <= layerMax; j++){
-                int layerMaxPrev = layerMaxP(nn, i - 1);
-                nn->neurons.items[j].wIndexStart = nn->weights.count;
-                for(int k = nn->layerIndexes.items[i - 1]; k <= layerMaxPrev; k++){
+    for(int i = 1; i < nn->layerIndexes.count; i++){
+        for(int j = nn->layerIndexes.items[i]; j <= layerMaxP(nn, i); j++){
+            int layerMaxPrev = layerMaxP(nn, i - 1);
+            nn->neurons.items[j].wIndexStart = nn->weights.count;
+            for(int k = nn->layerIndexes.items[i - 1]; k <= layerMaxPrev; k++){
                     printf("Connection - Layer(%d:%d) -> Layer(%d:%d)\n", i - 1, k, i, j);
-                    da_append(nn->weights, randM0505());
-                }
+                    da_append(nn->weights, randf(-0.5f, 0.5f));
             }
-    
         }
-        printf("\n");
     }
 
     nn->weightGradients.count = nn->weights.count;
@@ -223,56 +211,22 @@ void GenerateRandomNeuralNetwork(neuralNetwork *nn){
 }
 
 void neuralNetworkForward(neuralNetwork *nn, dataSample dS){
-    nn->inputLayer.items[0].value = dS.input1;
-    nn->inputLayer.items[0].valueAA = dS.input1;
+    nn->neurons.items[0].value = dS.input1;
+    nn->neurons.items[1].value = dS.input2;
 
-    nn->inputLayer.items[1].value = dS.input2;
-    nn->inputLayer.items[1].valueAA = dS.input2;
-
-    for(int i = 0; i <= nn->layerIndexes.count; i++){
-        if(i == nn->layerIndexes.count){
-            for(int j = 0; j < nn->outputLayer.count; j++){
-                float value = nn->outputLayer.items[j].bias;
-                int cWeight = nn->outputLayer.items[j].wIndexStart;
-                int layerMax = layerMaxP(nn, i - 1);
-                for(int k = nn->layerIndexes.items[i - 1]; k <= layerMax; k++){
-                    value += nn->neurons.items[k].valueAA * nn->weights.items[cWeight];
-                    cWeight++;
-                }
-                nn->outputLayer.items[j].value   = value;
-                nn->outputLayer.items[j].valueAA = activate(value, af_sigmoid);
+    for(int i = 1; i < nn->layerIndexes.count; i++){
+        for(int j = nn->layerIndexes.items[i]; j <= layerMaxP(nn, i); j++){
+            float value = nn->neurons.items[j].bias;
+            int cWeight = nn->neurons.items[j].wIndexStart;
+            for(int k = nn->layerIndexes.items[i - 1]; k <= layerMaxP(nn, i - 1); k++){
+                value += activate(&nn->neurons.items[k]) * nn->weights.items[cWeight];
+                cWeight++;
             }
-        } else if(i == 0){
-            int layerMax = layerMaxP(nn, i);
-            for(int j = nn->layerIndexes.items[i]; j <= layerMax; j++){
-                float value = nn->neurons.items[j].bias;
-                int cWeight = nn->neurons.items[j].wIndexStart;
-                for(int k = 0; k < nn->inputLayer.count; k++){
-                    value += nn->inputLayer.items[k].valueAA * nn->weights.items[cWeight];
-                    cWeight++;
-                }
-                nn->neurons.items[j].value   = value;
-                nn->neurons.items[j].valueAA = activate(value, af_ReLu);
-            }
-        }
-        else{
-            int layerMax = layerMaxP(nn, i);
-            for(int j = nn->layerIndexes.items[i]; j <= layerMax; j++){
-                float value = nn->neurons.items[j].bias;
-                int cWeight = nn->neurons.items[j].wIndexStart;
-                int layerMaxPrev = layerMaxP(nn, i - 1);
-                for(int k = nn->layerIndexes.items[i - 1]; k <= layerMaxPrev; k++){
-                    value += nn->neurons.items[k].valueAA * nn->weights.items[cWeight];
-                    cWeight++;
-                }
-                nn->neurons.items[j].value   = value;
-                nn->neurons.items[j].valueAA = activate(value, af_ReLu);
-            }
+            nn->neurons.items[j].value   = value;
         }
     }
 
-    
-    nn->prediction = nn->outputLayer.items[0].valueAA;
+    nn->prediction = activate(&nn->neurons.items[nn->neurons.count - 1]);
     nn->target     = dS.target; 
     nn->error      = nn->prediction - nn->target;
 
@@ -283,50 +237,23 @@ void neuralNetworkForward(neuralNetwork *nn, dataSample dS){
 }
 
 void neuralNetworkBackward(neuralNetwork *nn){
-    //This shit sucks and it's hardcoded as fuck
-    //I need to merge input and output layers into the same neuron blob
-    
-    //Output layer
-    for(int j = 0; j < nn->outputLayer.count; j++){
-        nn->outputLayer.items[j].gradient = nn->dOutput;
+    for(int i = nn->layerIndexes.count - 1; i > 0; i--){
+        if(i == nn->layerIndexes.count - 1){
+            for(int j = layerStartP(nn, i); j <= layerMaxP(nn, i); j++){
+                nn->neurons.items[j].gradient = nn->dOutput;
 
-        int cWeight = nn->outputLayer.items[j].wIndexStart;
-        int layerMax = layerMaxP(nn, nn->layerIndexes.count - 1);
-        for(int k = nn->layerIndexes.items[nn->layerIndexes.count - 1]; k <= layerMax; k++){
-            nn->weightGradients.items[cWeight] = nn->outputLayer.items[j].gradient * nn->neurons.items[k].valueAA;
-            cWeight++;
+                int cWeight = nn->neurons.items[j].wIndexStart;
+                for(int k = layerStartP(nn, i - 1); k <= layerMaxP(nn, i - 1); k++){
+                    nn->weightGradients.items[cWeight] = nn->neurons.items[j].gradient * activate(&nn->neurons.items[k]);
+                    cWeight++;
+                }
+            }
+            continue;
         }
-    }
-
-    //First layer after output
-    int layerMax = layerMaxP(nn, nn->layerIndexes.count - 1);
-    int cLayerNStart = nn->layerIndexes.items[nn->layerIndexes.count - 1];
-    for(int j = cLayerNStart; j <= layerMax; j++){
-        float cGradient = 0;
-        for(int k = 0; k < nn->outputLayer.count; k++){
-            int cWeight = nn->outputLayer.items[k].wIndexStart + (j - cLayerNStart);
-            cGradient += nn->outputLayer.items[k].gradient * nn->weights.items[cWeight];
-        }
-        cGradient = nn->neurons.items[j].value <= 0.0f ? 0 : cGradient;
-        nn->neurons.items[j].gradient = cGradient;
-
-        int layerMaxInner = layerMaxP(nn, nn->layerIndexes.count - 2);
-        int cWeight = nn->neurons.items[j].wIndexStart;
-
-        for(int k = nn->layerIndexes.items[nn->layerIndexes.count - 2]; k <= layerMaxInner; k++){
-            nn->weightGradients.items[cWeight] = cGradient * nn->neurons.items[k].valueAA;
-            cWeight++;
-        }
-    }
-
-    //last layer
-    for(int i = nn-> layerIndexes.count - 2; i >= 0; i--){
-        int layerMax = layerMaxP(nn, i);
-        for(int j = nn->layerIndexes.items[i]; j <= layerMax; j++){
-            int layerMaxInner = layerMaxP(nn, i + 1);
-
+        
+        for(int j = layerStartP(nn, i); j <= layerMaxP(nn, i); j++){
             float cGradient = 0;
-            for(int k = nn->layerIndexes.items[i + 1]; k <= layerMaxInner; k++){
+            for(int k = layerStartP(nn, i + 1); k <= layerMaxP(nn, i + 1); k++){
                 int cWeight = nn->neurons.items[k].wIndexStart + j - nn->layerIndexes.items[i];
                 cGradient += nn->neurons.items[k].gradient * nn->weights.items[cWeight];
             }
@@ -334,8 +261,8 @@ void neuralNetworkBackward(neuralNetwork *nn){
             nn->neurons.items[j].gradient = cGradient;
 
             int cWeight = nn->neurons.items[j].wIndexStart;
-            for(int k = 0; k < nn->inputLayer.count; k++){
-                nn->weightGradients.items[cWeight] = cGradient * nn->inputLayer.items[k].valueAA;
+            for(int k = layerStartP(nn, i - 1); k <= layerMaxP(nn, i - 1); k++){
+                nn->weightGradients.items[cWeight] = nn->neurons.items[j].gradient * activate(&nn->neurons.items[k]);
                 cWeight++;
             }
         }
@@ -343,10 +270,6 @@ void neuralNetworkBackward(neuralNetwork *nn){
 }
 
 void neuralNetworkUpdate(neuralNetwork *nn, float learningRate){
-    for(int i = 0; i < nn->outputLayer.count; i++){
-        nn->outputLayer.items[i].bias -= nn->outputLayer.items[i].gradient * learningRate;
-    }
-
     for(int i = 0; i < nn->neurons.count; i++){
         nn->neurons.items[i].bias -= nn->neurons.items[i].gradient * learningRate;
     }
@@ -359,22 +282,12 @@ void neuralNetworkUpdate(neuralNetwork *nn, float learningRate){
 void neuralNetworkPrint(neuralNetwork *nn){
     printf("Neural network: \n");
     printf("----Layers: \n");
-    printf("--------input layer(%d): \n", nn->inputLayer.count);
-    for(int i = 0; i < nn->inputLayer.count; i++){
-        printf("----------------|%f Value: %f(%f)\n", nn->inputLayer.items[i].bias, nn->inputLayer.items[i].valueAA, nn->inputLayer.items[i].value);
-    }
-
     for (int i = 0; i < nn->layerIndexes.count; i++){
         int layerMax = i == nn->layerIndexes.count - 1 ? (nn->neurons.count - 1) : (nn->layerIndexes.items[i + 1] - 1);
         printf("------------layer(%d)(%d): \n", i, layerMax);
         for(int j = nn->layerIndexes.items[i]; j <= layerMax; j++){
-            printf("----------------|%f Indexes from: %d Value: %f(%f)\n", nn->neurons.items[j].bias, nn->neurons.items[j].wIndexStart, nn->neurons.items[j].valueAA, nn->neurons.items[j].value);
+            printf("----------------|%f Indexes from: %d Value: %f(%f)\n", nn->neurons.items[j].bias, nn->neurons.items[j].wIndexStart, activate(&nn->neurons.items[j]), nn->neurons.items[j].value);
         }
-    }
-
-    printf("------------output layer(%d): \n", nn->outputLayer.count);
-    for(int i = 0; i < nn->outputLayer.count; i++){
-        printf("----------------|%f Indexes from: %d Value: %f(%f)\n", nn->outputLayer.items[i].bias, nn->outputLayer.items[i].wIndexStart, nn->outputLayer.items[i].valueAA, nn->outputLayer.items[i].value);
     }
 
     printf("----Weights: \n");
@@ -383,11 +296,11 @@ void neuralNetworkPrint(neuralNetwork *nn){
     }
 
     printf("Statistics: \n");
-    printf("----Prediction: %f\n", nn->outputLayer.items[0].valueAA);
+    printf("----Prediction: %f\n", nn->prediction);
     printf("----Target: %f\n", nn->target);
     printf("----Loss: %f\n", nn->loss);
     printf("----Output gradient: %f\n", nn->outputGradient);
-    printf("----Total space occupied by NN: %llub\n", (nn->inputLayer.count + nn->neurons.count + nn->outputLayer.count) * sizeof(float) + nn->weights.count * sizeof(float)); 
+    printf("----Total space occupied by NN: %llub\n", nn->neurons.count * sizeof(float) + nn->weights.count * sizeof(float)); 
     //Size of neuron counts as float because I only care about bias, value and wStart is not really important
 
     printf("Gradients: \n");
@@ -429,14 +342,18 @@ void neuralNetworkInteractive(neuralNetwork *nn){
             .input2 = input2,
             .target = 0.0f
         };
-
+        clock_t start = clock();
         neuralNetworkForward(nn, sample);
+        clock_t end = clock();
 
         float result = nn->prediction;
+
+        double elapsedTime = (double)(end - start) / CLOCKS_PER_SEC;
 
         printf("Input:      %.6f %.6f\n", input1, input2);
         printf("Prediction: %.6f\n", result);
         printf("Class:      %d\n", result >= 0.5f ? 1 : 0);
+        printf("Thinking time: %.6f seconds\n", elapsedTime);
     }
 
     printf("Interactive mode finished.\n");
@@ -456,12 +373,12 @@ int main(int argc, char* argv[]){
     printf("Generated %d data samples\n", tData.count);
 
     printf("Generating random neural network\n");
-    neuralNetwork nn = {.inputLayer = da_init(neuron), .neurons = da_init(neuron), .layerIndexes = da_init(int), .outputLayer = da_init(neuron), .weights = da_init(float)};
+    neuralNetwork nn = {.neurons = da_init(neuron), .layerIndexes = da_init(int), .weights = da_init(float)};
     GenerateRandomNeuralNetwork(&nn);
     printf("Original neural network: \n");
     neuralNetworkForward(&nn, (dataSample){.input1 = 0.0f, .input2 = 0.0f, .target = 0.0f});
     neuralNetworkPrint(&nn);
-    
+    clock_t start = clock();
     for(int epoch = 0; epoch < numEpochs; epoch++){
         for(int sample = 0; sample < tData.count; sample++){
             neuralNetworkForward(&nn, tData.items[sample]);
@@ -469,8 +386,13 @@ int main(int argc, char* argv[]){
             neuralNetworkUpdate(&nn, learningRate);
         }
     }
+    clock_t end = clock();
 
+    double elapsedTime = (double)(end - start) / CLOCKS_PER_SEC;
+    
     printf("Neural network after %d training steps: \n", numEpochs * tData.count);
+    printf("Training time: %.6f seconds\n", elapsedTime);
+    
     neuralNetworkForward(&nn, (dataSample){.input1 = 0.0f, .input2 = 0.0f, .target = 0.0f});
     neuralNetworkPrint(&nn);
 
